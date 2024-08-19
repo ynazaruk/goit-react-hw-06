c6b9761159827a92b261c16404e4d67268cc570f
ynazaruk <nazaruk.ya@gmail.com>
2024-08-20 00:13:53 +0300

    3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,13 @@ import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Searchbox from '../SearchBox/SearchBox';
 
-const App = () => (
+export default function App() {
+  return (
   <div>
     <h1>Phonebook</h1>
     <ContactForm />
-    <h2>Contacts</h2>
     <Searchbox />
     <ContactList />
   </div>
-);
-
-export default App;
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './components/App/App'
-import { Provider } from 'react-redux'
-import {store, persistor} from "./redux/store"
-import { PersistGate } from 'redux-persist/integration/react'
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import App from './components/App/App';
 
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-    <App />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
     </PersistGate>
-    </Provider>
-  </React.StrictMode>,
+  </Provider>
 );
